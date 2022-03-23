f10b534f6263cb4cbe1f9fa1da596543d6d01463
liulei <15019474951@163.com>
2022-03-23 20:55:22 +0800

    演示JDK代理模式

--- spring-study-aop/src/test/java/top/xiaorang/spring/aop/SpringTest.java
+++ spring-study-aop/src/test/java/top/xiaorang/spring/aop/SpringTest.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import top.xiaorang.spring.aop.proxy.staticproxy.TeacherService;
 
+import java.lang.reflect.Proxy;
+
 public class SpringTest {
   @Test
   public void test1() {
@@ -13,5 +15,27 @@ public class SpringTest {
     TeacherService teacherService =
         applicationContext.getBean("teacherServiceProxy", TeacherService.class);
     teacherService.teach();
+  }
+
+  @Test
+  public void test2() {
+    ApplicationContext applicationContext =
+        new ClassPathXmlApplicationContext("/applicationContext.xml");
+    TeacherService teacherService =
+        applicationContext.getBean("teacherService", TeacherService.class);
+    ClassLoader classLoader = teacherService.getClass().getClassLoader();
+    Class<?>[] interfaces = teacherService.getClass().getInterfaces();
+    TeacherService teacherServiceProxy =
+        (TeacherService)
+            Proxy.newProxyInstance(
+                classLoader,
+                interfaces,
+                (proxy, method, args) -> {
+                  System.out.println("---------log---------");
+                  Object res = method.invoke(teacherService, args);
+                  System.out.println("---------log---------");
+                  return res;
+                });
+    teacherServiceProxy.teach();
   }
 }
