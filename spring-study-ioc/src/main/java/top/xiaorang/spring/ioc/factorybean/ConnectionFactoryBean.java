0bc06bffbaa341aaa445e761ee17dd6b3e25b8ba
liulei <15019474951@163.com>
2022-09-13 14:10:33 +0800

    fix：use java spi load mysql driver

--- spring-study-ioc/src/main/java/top/xiaorang/spring/ioc/factorybean/ConnectionFactoryBean.java
+++ spring-study-ioc/src/main/java/top/xiaorang/spring/ioc/factorybean/ConnectionFactoryBean.java
@@ -6,56 +6,46 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactoryBean implements FactoryBean<Connection> {
-  private String driverClassName;
-  private String url;
-  private String username;
-  private String password;
+    private String url;
+    private String username;
+    private String password;
 
-  @Override
-  public Connection getObject() throws Exception {
-    Class.forName(driverClassName);
-    return DriverManager.getConnection(url, username, password);
-  }
+    @Override
+    public Connection getObject() throws Exception {
+        return DriverManager.getConnection(url, username, password);
+    }
 
-  @Override
-  public Class<?> getObjectType() {
-    return Connection.class;
-  }
+    @Override
+    public Class<?> getObjectType() {
+        return Connection.class;
+    }
 
-  @Override
-  public boolean isSingleton() {
-    return false;
-  }
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
 
-  public String getDriverClassName() {
-    return driverClassName;
-  }
+    public String getUrl() {
+        return url;
+    }
 
-  public void setDriverClassName(String driverClassName) {
-    this.driverClassName = driverClassName;
-  }
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-  public String getUrl() {
-    return url;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setUrl(String url) {
-    this.url = url;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
